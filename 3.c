c6b8d84abe0052f680c1e879d440fcae0e143f88
kinorego <kauan.morais.pereira@escola.pr.gov.br>
2025-03-12 12:25:28 -0300

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+
+  int main(){
+      int ano , bix, true;
+      scanf("%d" , &ano);
+      if(((ano% 4== 0) && (ano % 4== 0) && !(ano % 100 == 0))|| (ano % 100 == 400 == 0)){
+  
+          
+      }
+  
